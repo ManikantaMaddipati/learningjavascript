24459ac809ddf018de0d6971e6c8e592995a89b8
manikanta <manikanta3517@gmail.com>
2020-08-09 21:54:17 -0400

    //9.3.1 Static Methods

--- __tests__/classes.js
+++ __tests__/classes.js
@@ -126,9 +126,39 @@ test ("testing class",()=>{
     expect(h.xyz()).toBe("Hello World");
 })
 
+//9.3.1 Static Methods
+//Static methods are defined as properties of the constructor function rather than properties of the prototype object.
+//example: Because static methods are invoked on the constructor rather than on any particular instance,
+// it almost never makes sense to use the this keyword in a static method.
 
+class SaticRange {
+    constructor(from, to) {
+        // Store the start and end points (state) of this new range object.
+        // These are noninherited properties that are unique to this object.
+        this.from = from;
+        this.to = to;
+    }
+    static parse(s) {
+        let matches = s.match(/^\((\d+)\.\.\.(\d+)\)$/);
+        if (!matches) {
+            throw new TypeError(`Cannot parse Range from "${s}".`)
+        }
+        return new SaticRange(parseInt(matches[1]), parseInt(matches[2]));
+    }
 
+    // Return true if x is in the range, false otherwise
+    // This method works for textual and Date ranges as well as numeric.
+    includes(x) { return this.from <= x && x <= this.to; }
 
+    // A generator function that makes instances of the class iterable.
+    // Note that it only works for numeric ranges.
+    *[Symbol.iterator]() {
+        for(let x = Math.ceil(this.from); x <= this.to; x++) yield x;
+    }
+
+    // Return a string representation of the range
+    toString() { return `(${this.from}...${this.to})`; }
+}
 
 
 
